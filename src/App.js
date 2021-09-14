f1162cbcdcc7671f10d59acd63ff1c9893113047
ravenkmusic <ravenkbeats@gmail.com>
2021-09-14 16:27:18 -0400

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,16 +4,29 @@ import Search from "./Search";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather Application</h1>
+      <div className="container">
+        <h1>Weather Application</h1>
       <Search />
-      <p>
+      <footer>
         This application is{" "}
-        <a href="https://github.com/ravenkmusic/shecodes-weather-react">
+        <a
+          href="https://github.com/ravenkmusic/shecodes-weather-react"
+          target="_blank"
+          rel="noreferrer"
+        >
           open sourced
         </a>{" "}
-        by <a href="https://ravenkmusic.com">Raven Hendricks</a> and hosted through{" "}
-        <a href="https://netlify.com">Netlify</a>.
-      </p>
+        by{" "}
+        <a href="https://ravenkmusic.com" target="_blank" rel="noreferrer">
+          Raven Hendricks
+        </a>{" "}
+        and hosted through{" "}
+        <a href="https://netlify.com" target="_blank" rel="noreferrer">
+          Netlify
+        </a>
+        .
+      </footer>
+      </div>
     </div>
   );
 }
