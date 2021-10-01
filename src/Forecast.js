a1d46b280d1adc6d3cbe22034e2c63248e0770c6
ravenkmusic <ravenkbeats@gmail.com>
2021-09-30 22:30:49 -0400

    added degree symbol to temperatures for dummy data

--- src/Forecast.js
+++ src/Forecast.js
@@ -10,40 +10,40 @@ export default function Forecast() {
           <div className="fw-bold fs-5 weekday">Monday</div>
           <WeatherIcon code="01d" size="30" />
           <div className="forecast-max-min-temp">
-            <span className="forecast-temp-max">24</span>{" "}
-            <span className="forecast-temp-min">19</span>
+            <span className="forecast-temp-max">24°</span>{" "}
+            <span className="forecast-temp-min">19°</span>
           </div>
         </div>
         <div className="col">
           <div className="fw-bold fs-5 weekday">Tuesday</div>
           <WeatherIcon code="01d" size="30" />
           <div className="forecast-max-min-temp">
-            <span className="forecast-temp-max">24</span>{" "}
-            <span className="forecast-temp-min">19</span>
+            <span className="forecast-temp-max">24°</span>{" "}
+            <span className="forecast-temp-min">19°</span>
           </div>
         </div>
         <div className="col">
           <div className="fw-bold fs-5 weekday">Wednesday</div>
           <WeatherIcon code="01d" size="30" />
           <div className="forecast-max-min-temp">
-            <span className="forecast-temp-max">24</span>{" "}
-            <span className="forecast-temp-min">19</span>
+            <span className="forecast-temp-max">24°</span>{" "}
+            <span className="forecast-temp-min">19°</span>
           </div>
         </div>
         <div className="col">
           <div className="fw-bold fs-5 weekday">Thursday</div>
           <WeatherIcon code="01d" size="30" />
           <div className="forecast-max-min-temp">
-            <span className="forecast-temp-max">24</span>{" "}
-            <span className="forecast-temp-min">19</span>
+            <span className="forecast-temp-max">24°</span>{" "}
+            <span className="forecast-temp-min">19°</span>
           </div>
         </div>
         <div className="col">
           <div className="fw-bold fs-5 weekday">Friday</div>
           <WeatherIcon code="01d" size="30" />
           <div className="forecast-max-min-temp">
-            <span className="forecast-temp-max">24</span>{" "}
-            <span className="forecast-temp-min">19</span>
+            <span className="forecast-temp-max">24°</span>{" "}
+            <span className="forecast-temp-min">19°</span>
           </div>
         </div>
       </div>
